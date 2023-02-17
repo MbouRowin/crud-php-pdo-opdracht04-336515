6001c44ea52efe7cee60d02ea478183b18cf5851
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-02-17 11:04:33 +0100

    Change title

--- index.php
+++ index.php
@@ -33,13 +33,13 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Invoer Achtbaan</title>
+    <title>Bling Bling Nagelstudio Chantal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container">
-        <h1 class="text-center">Invoer Achtbaan</h1>
+        <h1 class="text-center">Bling Bling Nagelstudio Chantal</h1>
         <a href="read.php">Read</a>
 
         <form method="post">
